ef848ffa66ce1e6718d584aaa88d73c66fd4bd05
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2023-11-23 22:51:46 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 30.88 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,34 +1,25 @@
 def solution(queue1, queue2):
-    # 두 큐의 합을 계산
-    total = sum(queue1) + sum(queue2)
-    
-    # 총합이 홀수인 경우, 같은 합을 만들 수 없음
-    if total % 2 != 0:
+    answer = -2
+    all_que = queue1+ queue2
+    all_sum = sum(all_que)
+    if all_sum%2!=0:
         return -1
-
-    target = total // 2  # 목표 합
-    combined = queue1 + queue2  # 두 큐를 연결
-    length = len(combined)
-    sum_current = sum(queue1)
-    start, end = 0, len(queue1)  # 시작점과 끝점
-
-    for count in range(2 * length):
-        if sum_current == target:
-            return count
-        if sum_current > target:
-            if start < length:
-                sum_current -= combined[start]
-                start += 1
+    target = all_sum//2
+    start,end = 0, len(queue1)
+    target_sum = sum(queue1)
+    max_len = len(all_que)
+    for i in range(2*len(all_que)):
+        if target == target_sum:
+            return i
+        if target < target_sum:
+            if max_len<=start:
+                break
+            target_sum -= all_que[start]
+            start+=1
         else:
-            if end < length:
-                sum_current += combined[end]
-                end += 1
-            else:
-                break  # 리스트 범위를 벗어나면 반복 종료
-
+            if max_len<=end:
+                break
+            target_sum += all_que[end]
+            end +=1
+        
     return -1
-
-# # 예시 실행
-# print(solution([3, 2, 7, 2], [4, 6, 5, 1]))  # 결과: 2
-# print(solution([1, 2, 1, 2], [1, 10, 1, 2]))  # 결과: 7
-# print(solution([1, 1], [1, 5]))  # 결과: -1
