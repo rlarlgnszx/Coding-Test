a65f1b1786c4421502e4960e64aba74def69de32
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2024-04-09 14:18:29 +0900

    [Gold IV] Title: 부분합, Time: 136 ms, Memory: 120932 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,26 +1,30 @@
 import sys
+input =sys.stdin.readline
 
-N, S = map(int, input().split())
-numbers = list(map(int, input().split()))
 
-left, right = 0, 0 # 두 개의 포인터는 0에서 부터 시작
-sum = 0 # 합을 저장할 변수
-min_length = sys.maxsize # 먼저 최대 길이로 지정
+n,s = map(int,input().split())
 
-while True:
-    # 만약 총 합이 S가 넘는다면, left를 하나씩 옮겨보면서 어디까지 길이가 줄어드나 확인
-    if sum >= S:
-        min_length = min(min_length, right - left)
-        sum -= numbers[left]
-        left += 1
-    elif right == N:
+numbers = list(map(int,input().split()))
+min_size =sys.maxsize
+
+sum_numbers=  0
+left,right = 0,0
+
+while 1:
+    if sum_numbers >= s:
+        min_size = min(min_size,right-left)
+        sum_numbers -= numbers[left]
+        left +=1
+        
+    elif right==n:
         break
-    # 만약 총합이 S를 넘지 않는다면, right 을 오른쪽으로 한칸씩 옮기며 총합이 S를 넘을때까지 더함
     else:
-        sum += numbers[right]
-        right += 1
+        sum_numbers+= numbers[right]
+        right+=1
+## dp i,j 는 i~부터 j까지의 최솟값
 
-if min_length == sys.maxsize:
+
+if min_size==sys.maxsize:
     print(0)
 else:
-    print(min_length)
+    print(min_size)
