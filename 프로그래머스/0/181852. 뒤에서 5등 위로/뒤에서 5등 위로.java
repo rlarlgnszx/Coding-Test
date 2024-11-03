418e0a90c78248624f888be5cd0b1253cb3ea9dc
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2024-11-03 17:21:36 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 10.36 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,8 +1,9 @@
 import java.util.*;
-
+import java.util.stream.*;
+ 
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = {};
-        return Arrays.stream(num_list).sorted().skip(5).toArray();
+        return IntStream.of(num_list).sorted().skip(5).toArray();
     }
 }
