7f53ae46ba506e2116968f2175ad10bb37d8885e
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2024-11-03 17:07:05 +0900

    [level 0] Title: 정수 부분, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181850. 정수 부분/정수 부분.java
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(double flo) {
-        return (int) Math.floor(flo);
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.solution(1.42));  // 출력: 1
-        System.out.println(sol.solution(69.32)); // 출력: 69
+        return (int) flo;
     }
 }
