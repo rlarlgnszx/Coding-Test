cf54fb71b3d0cd104d3503111fec5da8c8fc6b70
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2024-11-04 21:11:16 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.44 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] arr) {
+        int answer = 0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]!=arr[j][i]){
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}
